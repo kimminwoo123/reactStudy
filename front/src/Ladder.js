65a1c2e98a95c1d054e4de73d4a0d06bb114d91b
gkfnzl <gkfnzl@ilscp.net>
2020-12-23 17:38:05 +0900

    2020/12/23

--- front/src/Ladder.js
+++ front/src/Ladder.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-// import { Modal, Button } from 'antd'
+import { Modal, Button, Alert } from 'antd'
 import '../node_modules/antd/dist/antd.css'
 
 import { Main, ScreenShot } from './main'
@@ -7,29 +7,22 @@ import { Main, ScreenShot } from './main'
 
 const Ladder = () => {
 
-    // const [visible, setVisible] = useState(false)
+    const [visible, setVisible] = useState(false)
+
+    const handleClose = () => {
+        setVisible(false)
+    }
 
     useEffect(() => {
-
-        // const divBody = document.createElement('div')
-
-        // divBody.setAttribute('id', 'div_body')
-
-        // const divParent = document.getElementsByName('div_body_parents')
-
-        // divParent.appendChild(divBody)
-
         const ladderLogic = async () => { // 사다리 로직
             await Main()
         }
 
         ladderLogic().then(() => {
             ScreenShot()
+            setVisible(true)
         })
 
-        // window.onbeforeunload = () => { // 새로고침 방지
-        //     return false
-        // }
     }, [])
 
     return (
@@ -37,18 +30,9 @@ const Ladder = () => {
             <div id="div_body_parents">
                 <div id="div_body"></div>
             </div>
-            {/* <Button type="primary" onClick={() => setVisible(true)}>
-                Open Modal of 1000px width
-            </Button>
-            <Modal
-                title="Modal 1000px width"
-                centered
-                visible={visible}
-                onOk={() => setVisible(false)}
-                onCancel={() => setVisible(false)}
-                width={1000}
-            >
-            </Modal> */}
+            {visible ? (
+                <Alert message="Success" type="success" closable afterClose={handleClose} />
+            ) : null}
         </>
     )
 }
